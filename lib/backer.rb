83ff902fa8b9740ce10fcf51e9453652f60a83ff
Ron Sala <ron.sala@gmail.com>
2018-02-04 14:21:16 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,9 @@
 class Backer
   attr_reader :name, :backed_projects
-  # BACKERS = []
 
   def initialize(name)
     @name = name
     @backed_projects = []
-    # BACKERS << self
   end
 
   def back_project(project)
