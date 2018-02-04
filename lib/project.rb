8547ecfc348b36514d3555c6e1a2f0eb2da6587b
Ron Sala <ron.sala@gmail.com>
2018-02-04 14:24:07 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,17 @@
+# class Project
+#   attr_reader :title, :backers
+# 
+#   def initialize(title)
+#     @title = title
+#     @backers = []
+#   end
+# 
+#   def add_backer(backer)
+#     @backers << backer
+#     backer.back_project(self) unless backer.backed_projects.include?(self)
+#   end
+# end
+
 class Project
   attr_reader :title, :backers
 
@@ -7,7 +21,8 @@ class Project
   end
 
   def add_backer(backer)
-    @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+    backers << backer
+    backer.backed_projects << self
   end
+
 end
